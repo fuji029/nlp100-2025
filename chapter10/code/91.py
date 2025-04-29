5b1f50685a8ccf6a20fb2fcda26dd7a7d4902d23
Yuki Fujiwara <fuzi1130sw@gmail.com>
2025-04-29 11:26:39 +0900

    bugfix

--- chapter10/code/91.py
+++ chapter10/code/91.py
@@ -1,12 +1,16 @@
 from transformers import pipeline, TextGenerationPipeline
+import pandas as pd
 import os
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 generator: TextGenerationPipeline = pipeline(model="openai-community/gpt2")
-print("temperature\tgenerated_text")
+result = []
 for temperature in range(2, 10, 2):
-    outputs = generator(
+    generated_text = generator(
         "The movie was full of",
         temperature=temperature / 10)[0]['generated_text'].replace('\n', ' ')
-    print(f"{temperature/10}\t{outputs}")
+    result.append({"temperature": temperature/10,
+                  "generated_text": generated_text})
+
+pd.DataFrame(result).to_csv("out/91.tsv", sep="\t")
